afca7531e1fdc8576f2a7381603173fee266fec8
MrMathi <mathias-schiestl@hotmail.de>
2019-10-16 17:41:48 +0200

    finished pi_seq and started with pi_mpi

--- proseminar/02/ex1/pi_mpi.c
+++ proseminar/02/ex1/pi_mpi.c
@@ -1,23 +1,33 @@
 #include<mpi.h>
 #include<stdio.h>
 
-intmain(intargc,char**argv){
-    MPI_Init(&argc,&argv);//initializetheMPIenvironment
-    intsize;
-    MPI_Comm_size(MPI_COMM_WORLD,&size); //getthe numberofranks
-    intrank;
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank); //getthe rankofthecaller
-    printf("Helloworldfromrank%dof%d\n",rank,size);
+int main(intargc,char**argv){
+    MPI_Init(&argc,&argv);  //initialize the MPI environment
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD,&size); //get the number of ranks
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank); //get the rankof the caller
+    printf("Hello world from rank %d of %d\n",rank,size);
     double random_value;
     srand ( time ( NULL));
-    int gen,hits = 0;
-    int MAX_GEN = 1000000000;
-    for (;gen<MAX_GEN;gen++) {
-        random_value = (double)rand()/RAND_MAX*sqrt(2); //float in range 0 to sqaure root of 2
-        hits = (random_value > 1) ? hits : hits++;
+    unsigned long int gen, hits = 0;
+    unsigned long int MAX_GEN_ELEMENTS = 1000000000;
+    int num_gen_elements_per_proc = MAX_GEN_ELEMENTS/size
+    for (; gen < num_gen_elements_per_proc; gen++) {
+        // https://itp.tugraz.at/MML/MonteCarlo/MCIntro.pdf
+        double random_x = (double)rand()/RAND_MAX; //float in range 0 to 1
+        double random_y = (double)rand()/RAND_MAX;
+        double random_point = pow(random_x, 2) + pow(random_y, 2);
+        hits = (random_point < 1) ? ++hits : hits;
     }
-    double pi = hits/gen;
-    // TODO Send to rank 1
-    // TODO Make avarage of all
-    MPI_Finalize();//cleanup
+    // Print the hits on each process
+    printf("Local hits for process %d - hits %d - gens %d\n", rank, hits, gen);
+
+    // Reduce all of the local hits into the global hits
+    int global_hits = 0;
+    MPI_Reduce(&hits, &global_hits, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("PI = %f\n", global_hits / (size * num_gen_elements_per_proc));
+    }
+    MPI_Finalize(); //cleanup
 }
--- proseminar/02/ex1/pi_seq.c
+++ proseminar/02/ex1/pi_seq.c
@@ -4,19 +4,17 @@
 #include <math.h>
 #include <time.h>
 
-int main(){
+int main(int argc, char* argv[]){
     srand ( time ( NULL));
-    int gen,hits = 0;
-    long int MAX_GEN = 1000000000;
-    for (;gen<MAX_GEN;gen++) {
+    int gen, hits = 0;
+    long int MAX_GEN_ELEMENTS = 1000000000;
+    for (; gen < MAX_GEN_ELEMENTS; gen++) {
         // https://itp.tugraz.at/MML/MonteCarlo/MCIntro.pdf
         double random_x = (double)rand()/RAND_MAX; //float in range 0 to 1
         double random_y = (double)rand()/RAND_MAX;
-        double random_point = pow(random_x,2) + pow(random_y,2);
+        double random_point = pow(random_x, 2) + pow(random_y, 2);
         hits = (random_point < 1) ? ++hits : hits;
     }
     double pi = ((double)hits/(double)gen)*4;
-    printf("%d %d %f",hits, gen, pi);
-    // TODO Send to rank 1
-    // TODO Make avarage of all
+    printf("PI =  %f\n", pi);
 }
