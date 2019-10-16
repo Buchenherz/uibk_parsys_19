2355cf8b9de2950661182c154d610b1520c7675f
MrMathi <mathias-schiestl@hotmail.de>
2019-10-16 14:51:24 +0200

    fixed pi_seq

--- proseminar/02/ex1/pi_seq.c
+++ proseminar/02/ex1/pi_seq.c
@@ -5,15 +5,17 @@
 #include <time.h>
 
 int main(){
-    double random_value;
     srand ( time ( NULL));
     int gen,hits = 0;
-    long long int MAX_GEN = 10000000000;
+    long int MAX_GEN = 1000000000;
     for (;gen<MAX_GEN;gen++) {
-        random_value = (double)rand()/RAND_MAX*sqrt(2); //float in range 0 to sqaure root of 2
-        hits = (random_value > 1) ? hits : ++hits;
+        // https://itp.tugraz.at/MML/MonteCarlo/MCIntro.pdf
+        double random_x = (double)rand()/RAND_MAX; //float in range 0 to 1
+        double random_y = (double)rand()/RAND_MAX;
+        double random_point = pow(random_x,2) + pow(random_y,2);
+        hits = (random_point < 1) ? ++hits : hits;
     }
-    double pi = ((double)hits/(double)gen) *4;
+    double pi = ((double)hits/(double)gen)*4;
     printf("%d %d %f",hits, gen, pi);
     // TODO Send to rank 1
     // TODO Make avarage of all
