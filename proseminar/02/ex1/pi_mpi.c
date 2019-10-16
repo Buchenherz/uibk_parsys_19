039c49c0d59d2b0450fecdcc1e9bdc466dc0f1b1
LV 703651 - PS Parallele Systeme <cb761038@login.intern.lcc2.uibk.ac.at>
2019-10-16 21:05:11 +0200

    fixed pi_mpi.script; pi_mpi.c REDUCE wrong

--- proseminar/02/ex1/pi_mpi.c
+++ proseminar/02/ex1/pi_mpi.c
@@ -1,7 +1,10 @@
 #include<mpi.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
-int main(intargc,char**argv){
+int main(int argc,char**argv){
     MPI_Init(&argc,&argv);  //initialize the MPI environment
     int size;
     MPI_Comm_size(MPI_COMM_WORLD,&size); //get the number of ranks
@@ -12,7 +15,7 @@ int main(intargc,char**argv){
     srand ( time ( NULL));
     unsigned long int gen, hits = 0;
     unsigned long int MAX_GEN_ELEMENTS = 1000000000;
-    int num_gen_elements_per_proc = MAX_GEN_ELEMENTS/size
+    int num_gen_elements_per_proc = MAX_GEN_ELEMENTS/size;
     for (; gen < num_gen_elements_per_proc; gen++) {
         // https://itp.tugraz.at/MML/MonteCarlo/MCIntro.pdf
         double random_x = (double)rand()/RAND_MAX; //float in range 0 to 1
@@ -27,7 +30,8 @@ int main(intargc,char**argv){
     int global_hits = 0;
     MPI_Reduce(&hits, &global_hits, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
     if (rank == 0) {
-        printf("PI = %f\n", global_hits / (size * num_gen_elements_per_proc));
+        long int all_generated = size * num_gen_elements_per_proc;
+        printf("PI = %f\n (All hits: %d | Genterated Points: %d)", (double)global_hits*4 / (double)all_generated, global_hits,all_generated);
     }
     MPI_Finalize(); //cleanup
 }
