1402217cfd5b0400f4dae78c2a007179fb4590ec
Clemens Pfister <clemens.pfister@student.uibk.ac.at>
2019-11-04 14:39:00 +0100

    3d heat stencil mpi for planes
    + same output as sequential version
    + seems good

--- proseminar/03/heat_stencil_3D_mpi.c
+++ proseminar/03/heat_stencil_3D_mpi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <stdbool.h>
 
 typedef double value_t;
 
@@ -29,17 +30,17 @@ void printTemperature(Room m, int N, int M, int H);
 int main(int argc, char **argv)
 {
   // 'parsing' optional input parameter = problem size
-  int N = 96; // columns
-  int M = 48; // planes
-  int H = 12; // height
+  int Nx = 10; // columns
+  int Ny = 10; // planes
+  int Nz = 10; // height
   if (argc > 1)
   {
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
-    H = atoi(argv[3]);
+    Nx = atoi(argv[1]);
+    Ny = atoi(argv[2]);
+    Nz = atoi(argv[3]);
   }
-  int max = (N < M ? M : N);
-  int T = (max < H ? H : max) * 500;
+  int max = (Nx < Ny ? Ny : Nx);
+  int T = (max < Nz ? Nz : max) * 500;
 
   // region OPENMPI INIT
   int number_of_ranks;
@@ -50,15 +51,15 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);            //get the rankof the caller
 
   MPI_Datatype plane;
-  MPI_Type_vector(M, N, 1, MPI_DOUBLE, &plane);
+  MPI_Type_vector(Nx, Nz, 1, MPI_DOUBLE, &plane);
   MPI_Type_commit(&plane);
 
-  MPI_Datatype myType;
-  int size[3] = {H, M, N};
-  int subSize[3] = {H, M, N};
+  MPI_Datatype subarrayType;
+  int size[3] = {Nz, Ny, Nx};
+  int subSize[3] = {Nz, Ny, Nx};
   int start[3] = {0, 0, 0};
-  MPI_Type_create_subarray(3, size, subSize, start, MPI_ORDER_C, MPI_DOUBLE, &myType);
-  MPI_Type_commit(&myType);
+  MPI_Type_create_subarray(3, size, subSize, start, MPI_ORDER_C, MPI_DOUBLE, &subarrayType);
+  MPI_Type_commit(&subarrayType);
   // Cartesian communicator
   MPI_Comm comm_cart;
   int ndims = 2;
@@ -77,10 +78,10 @@ int main(int argc, char **argv)
 
   if (rank == 0)
   {
-    printf("Computing heat-distribution for room size N=%d, M=%d for T=%d timesteps\n", N, M, T);
+    printf("Computing heat-distribution for room size N=%d, M=%d for T=%d timesteps\n", Nx, Ny, T);
   }
 
-  int *rank_plane_sizes = getPlaneSizePerRank(H, number_of_ranks);
+  int *rank_plane_sizes = getPlaneSizePerRank(Ny, number_of_ranks);
 
   for (int i = 0; i < number_of_ranks; i++)
   {
@@ -90,14 +91,14 @@ int main(int argc, char **argv)
   // ---------- setup ----------
 
   // create a buffer for storing temperature fields
-  Room A = createRoom(N, M, H);
+  Room A = createRoom(Nx, Ny, Nz);
 
   // set up initial conditions in A
-  for (int i = 0; i < H; i++)
+  for (int i = 0; i < Nx; i++)
   {
-    for (int j = 0; j < M; j++)
+    for (int j = 0; j < Ny; j++)
     {
-      for (int k = 0; k < N; k++)
+      for (int k = 0; k < Nz; k++)
       {
         A[i][j][k] = 273; // temperature is 0° C everywhere (273 K)
       }
@@ -105,19 +106,19 @@ int main(int argc, char **argv)
   }
 
   // and there is a heat source in one corner
-  int source_x = N / 4;
-  int source_y = M / 8;
-  int source_z = H / 2;
+  int source_x = Nx / 4;
+  int source_y = Ny / 8;
+  int source_z = Nz / 2;
   A[source_z][source_y][source_x] = 273 + 60;
 
   printf("Initial:\t");
   // saveTemperature(A, N, M);
-  printTemperature(A, N, M, H);
+  // printTemperature(A, N, M, H);
   printf("\n");
 
   // ---------- compute ----------
   // create a second buffer for the computation
-  Room B = createRoom(N, M, H);
+  Room B = createRoom(Nx, Ny, Nz);
 
   // Set start and end plane for each rank
   // rank 0 starts from 0 and goes to rrs[1] - 1
@@ -149,82 +150,46 @@ int main(int argc, char **argv)
   {
     // send / receive planes here
     // planes that will be added on top / below local planes
-    Matrix received_upper_plane = createMatrix(N,M);
-    Matrix received_lower_plane = createMatrix(N,M);
+    Matrix received_upper_plane = createMatrix(Nx, Nz);
+    Matrix received_lower_plane = createMatrix(Nx, Nz);
 
     if (upper_rank < 0)
     {
       /* Upper rank does not exist, only receive one from below and send one to below */
-      MPI_Send(A[local_last_plane_index], 1, plane, lower_rank, 0, comm_cart);
-      MPI_Recv(received_lower_plane, 1, plane, lower_rank, 0, comm_cart, MPI_STATUS_IGNORE);
+      MPI_Send(&(A[local_last_plane_index][0][0]), Nx * Nz, MPI_DOUBLE, lower_rank, 0, comm_cart);
+      MPI_Recv(&(received_lower_plane[0][0]), Nx * Nz, MPI_DOUBLE, lower_rank, 0, comm_cart, MPI_STATUS_IGNORE);
 
       /* Received the last local plane from the lower rank, appending it as the new last local plane */
       A[local_last_plane_index + 1] = received_lower_plane;
-      for (int i = 0; i < M; i++)
-      {
-        for (int j = 0; j < N; j++)
-        {
-          printf("Recv: %lf, ", received_lower_plane[i][j]);
-        }
-        printf("\n");
-      }
-      printf("\n");
     }
     else if (lower_rank < 0)
     {
+
       /* Lower rank does not exist, only receive one from above and send one to above */
-      MPI_Recv(received_upper_plane, 1, plane, upper_rank, 0, comm_cart, MPI_STATUS_IGNORE);
-      MPI_Send(A[local_first_plane_index], 1, plane, upper_rank, 0, comm_cart);
+      MPI_Recv(&(received_upper_plane[0][0]), Nx * Nz, MPI_DOUBLE, upper_rank, 0, comm_cart, MPI_STATUS_IGNORE);
+      MPI_Send(&(A[local_first_plane_index][0][0]), Nx * Nz, MPI_DOUBLE, upper_rank, 0, comm_cart);
 
       /* Received the first local plane from the upper rank, appending it as the new last local plane */
       A[local_first_plane_index - 1] = received_upper_plane;
-      for (int i = 0; i < M; i++)
-      {
-        for (int j = 0; j < N; j++)
-        {
-          printf("Recv: %lf, ", received_upper_plane[i][j]);
-        }
-        printf("\n");
-      }
-      printf("\n");
     }
     else
     {
       MPI_Request request;
       /* Upper and lower ranks exist, send and receive both */
-      MPI_Isend(A[local_first_plane_index], 1, plane, upper_rank, 0, comm_cart, &request);
-      MPI_Isend(A[local_last_plane_index], 1, plane, lower_rank, 0, comm_cart, &request);
-      MPI_Recv(received_upper_plane, 1, plane, upper_rank, 0, comm_cart, MPI_STATUS_IGNORE);
-      MPI_Recv(received_lower_plane, 1, plane, lower_rank, 0, comm_cart, MPI_STATUS_IGNORE);
+      MPI_Isend(A[local_first_plane_index], Nx * Nz, MPI_DOUBLE, upper_rank, 0, comm_cart, &request);
+      MPI_Isend(A[local_last_plane_index], Nx * Nz, MPI_DOUBLE, lower_rank, 0, comm_cart, &request);
+      MPI_Recv(&(received_upper_plane[0][0]), Nx * Nz, MPI_DOUBLE, upper_rank, 0, comm_cart, MPI_STATUS_IGNORE);
+      MPI_Recv(&(received_lower_plane[0][0]), Nx * Nz, MPI_DOUBLE, lower_rank, 0, comm_cart, MPI_STATUS_IGNORE);
       A[local_first_plane_index - 1] = received_upper_plane;
       A[local_last_plane_index + 1] = received_lower_plane;
-      for (int i = 0; i < M; i++)
-      {
-        for (int j = 0; j < N; j++)
-        {
-          printf("Recv: %lf, ", received_lower_plane[i][j]);
-        }
-        printf("\n");
-      }
-      printf("\n");
-      for (int i = 0; i < M; i++)
-      {
-        for (int j = 0; j < N; j++)
-        {
-          printf("Recv: %lf, ", received_upper_plane[i][j]);
-        }
-        printf("\n");
-      }
-      printf("\n");
     }
-    releaseMatrix(received_upper_plane);
-    releaseMatrix(received_lower_plane);
+
     // .. we propagate the temperature
-    for (long long i = 0; i < H; i++)
+    for (long long i = 0; i < Nx; i++)
     {
-      for (long long j = 0; j < M; j++)
+      for (long long j = 0; j < Ny; j++)
       {
-        for (long long k = 0; k < N; k++)
+        for (long long k = 0; k < Nz; k++)
         {
           // center stays constant (the heat is still on)
           if (i == source_z && j == source_y && k == source_x)
@@ -237,15 +202,15 @@ int main(int argc, char **argv)
           value_t tc = A[i][j][k];
 
           // get temperatures of adjacent cells
-          value_t tl = (k != 0) ? A[i][j][k - 1] : tc;      // left
-          value_t tr = (k != N - 1) ? A[i][j][k + 1] : tc;  // right
-          value_t tu = (j != 0) ? A[i][j - 1][k] : tc;      // up
-          value_t td = (j != M - 1) ? A[i][j + 1][k] : tc;  // down
-          value_t th = (i != 0) ? A[i - 1][j][k] : tc;      // higher
-          value_t tlo = (i != H - 1) ? A[i + 1][j][k] : tc;  // lower
+          value_t tl = (k != 0) ? A[i][j][k - 1] : tc;       // left
+          value_t tr = (k != Nx - 1) ? A[i][j][k + 1] : tc;  // right
+          value_t tu = (j != 0) ? A[i][j - 1][k] : tc;       // up
+          value_t td = (j != Ny - 1) ? A[i][j + 1][k] : tc;  // down
+          value_t th = (i != 0) ? A[i - 1][j][k] : tc;       // higher
+          value_t tlo = (i != Nz - 1) ? A[i + 1][j][k] : tc; // lower
 
           // compute new temperature at current position
-          B[i][j][k] = tc + 0.1 * (tl + tr + tu + td +  th +  tlo + (-6 * tc));
+          B[i][j][k] = tc + 0.1 * (tl + tr + tu + td + th + tlo + (-6 * tc));
         }
       }
     }
@@ -255,74 +220,113 @@ int main(int argc, char **argv)
     A = B;
     B = Help;
 
-    // // show intermediate step
-    // if (!(t % 1000))
-    // {
-    //   if (rank == 0) {
-    //     Room comb = createRoom(N, M, H);
+    // show intermediate step
+    if (!(t % 1000))
+    {
+      if (rank == 0)
+      {
+        Room comb = createRoom(Nx, Ny, Nz);
 
-    //     for (int i = 0, i_rank = -1; i < H; i++)
-    //     {
-    //       if (i % local_m_end) {
-    //         i_rank++;
-    //       }
-    //       if (i < local_m_end) {
-    //         comb[i] = A[i];
-    //       }
-    //       else {
-    //         MPI_Recv(comb[i], 1, plane, i_rank , 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    //       }
-    //     }
-    //     printTemperature(comb, N, M, H);
-    //     releaseRoom(comb);
-    //   }
-    //   else
-    //   {
-    //     // printf("\nSend array %d\n", rank);
-    //     //MPI_Request req;
-    //     for (int i = local_m_start; i < local_m_end; i++) {
-    //       MPI_Send(A[i], 1, plane, 0, 1, MPI_COMM_WORLD);
-    //     //MPI_Wait(&req, MPI_STATUS_IGNORE);
-    //     }
-    //   }
-    // }
+        for (size_t i = 0; i < local_m_end; i++)
+        {
+          comb[i] = A[i];
+        }
+        // sleep(1);
+        for (int orank = 1; orank < number_of_ranks; orank++)
+        {
+          // printf("\n");
+          Room recv = createRoom(Nx, Ny, Nz);
+          // printf("\nRecv array %d\n", orank);
+          MPI_Recv(&(recv[0][0][0]), Nx * Nz * Ny, MPI_DOUBLE, orank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+          int sum = 0;
+          for (int j = 0; j < orank; j++)
+          {
+            sum = sum + rank_plane_sizes[j];
+          }
+          int o_m_start = sum;
+          int o_m_end = sum + rank_plane_sizes[orank];
+          // printf("omstart %d, omend %d\n", o_m_start, o_m_end);
+          for (int k = o_m_start; k < o_m_end; k++)
+          {
+            for (size_t g = 0; g < Ny; g++)
+            {
+              //printf("r: %f ", recv[k][g]);
+              comb[k][g] = recv[k][g];
+              /* code */
+            }
+            // printf("\n");
+          }
+        }
+        printTemperature(comb, Nx, Ny, Nz);
+        releaseRoom(comb);
+      }
+      else
+      {
+        // printf("\nSend array %d\n", rank);
+        //MPI_Request req;
+        MPI_Send(&(A[0][0][0]), Nx * Nz * Ny, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+      }
+    }
   }
 
   releaseRoom(B);
 
   // ---------- check ----------
-  int success = 1;
-  if (rank == 0) {
-    // Room comb = createRoom(N, M, H);
+  int success = true;
+  if (rank == 0)
+  {
+    Room comb = createRoom(Nx, Ny, Nz);
 
-    // for (int i = 0, i_rank = -1; i < H; i++)
-    // {
-    //   if (i % local_m_end) {
-    //     i_rank++;
-    //   }
-    //   if (i < local_m_end) {
-    //     comb[i] = A[i];
-    //   }
-    //   else {
-    //     MPI_Recv(comb[i], 1, plane, i_rank , 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    //   }
-    // }
+    for (size_t i = 0; i < local_m_end; i++)
+    {
+      comb[i] = A[i];
+    }
+    // sleep(1);
+    for (int orank = 1; orank < number_of_ranks; orank++)
+    {
+      // printf("\n");
+      Matrix recv = createMatrix(Nx, Nz);
+      // printf("\nRecv array %d\n", orank);
+      MPI_Recv(&(recv[0][0]), 1, subarrayType, orank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      int sum = 0;
+      for (int j = 0; j < orank; j++)
+      {
+        sum = sum + rank_plane_sizes[j];
+      }
+      int o_m_start = sum;
+      int o_m_end = sum + rank_plane_sizes[orank];
+      // printf("omstart %d, omend %d\n", o_m_start, o_m_end);
+      for (int k = o_m_start; k < o_m_end; k++)
+      {
+        for (size_t g = 0; g < Ny; g++)
+        {
+          //printf("r: %f ", recv[k][g]);
+          comb[k][g] = &recv[k][g];
+          /* code */
+        }
+        // printf("\n");
+      }
+    }
+
+    A[0][0][0] = comb[0][0][0];
 
     printf("Final:\n");
-    printTemperature(A, N, M, H);
+    printTemperature(A, Nx, Ny, Nz);
     printf("\n");
 
-    for (long i = 0; i < H; i++)
+    for (long i = 0; i < Nz; i++)
     {
-      for (long j = 0; j < M; j++)
+      for (long j = 0; j < Ny; j++)
       {
-        for (long k = 0; k < N; k++)
+        for (long k = 0; k < Nx; k++)
         {
           value_t temp = A[i][j][k];
+          printf("%lf ", A[i][j][k]);
           if (273 <= temp && temp <= 273 + 60)
             continue;
           success = 0;
-          printf("[%ld,%ld,%ld]",i,j,k);
           break;
         }
       }
@@ -332,18 +336,20 @@ int main(int argc, char **argv)
 
     // releaseRoom(comb);
   }
-//   else
-//   {
-//     // printf("\nSend array %d\n", rank);
-//     //MPI_Request req;
-//     for (int i = local_m_start; i < local_m_end; i++)
-//       MPI_Send(A[i], 1, plane, 0, 1, MPI_COMM_WORLD);
-//     //MPI_Wait(&req, MPI_STATUS_IGNORE);
-//   }
+  else
+  {
+    // printf("\nSend array %d\n", rank);
+    //MPI_Request req;
+    MPI_Send(&(A[0][0][0]), Nx * Nz, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+  }
 
   // ---------- cleanup ----------
 
   releaseRoom(A);
+  free(rank_plane_sizes);
+  MPI_Type_free(&plane);
+  MPI_Type_free(&subarrayType);
+  MPI_Finalize();
 
   // done
   return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
@@ -351,14 +357,17 @@ int main(int argc, char **argv)
 
 Room createRoom(int N, int M, int H)
 {
+  // https: //stackoverflow.com/questions/39108092/allocating-contiguous-memory-for-a-3d-array-in-c
   // create data and index Matrix
-  value_t ***room = malloc(sizeof(value_t) * H);
-  for (int i = 0; i < H ; i++)
+  value_t ***room = (value_t ***)malloc(sizeof(value_t **) * H);
+  value_t *data = (value_t *)malloc(sizeof(value_t) * N * M * H);
+  for (int i = 0; i < H; i++)
   {
-    room[i] = malloc(sizeof(value_t) * M);
-    for (int j = 0; j < M ; j++)
+    room[i] = (value_t **)malloc(sizeof(value_t *) * M);
+    for (int j = 0; j < M; j++)
     {
-      room[i][j] = malloc(sizeof(value_t) * N);
+      int idx = N * j + i * N * M;
+      room[i][j] = &data[idx];
     }
   }
   return room;
@@ -366,16 +375,21 @@ Room createRoom(int N, int M, int H)
 
 Matrix createMatrix(int N, int M)
 {
+  // https://stackoverflow.com/questions/5901476/sending-and-receiving-2d-array-over-mpi
   // create data and index Matrix
-  value_t **mat = malloc(sizeof(value_t) * M);
-  for (int i = 0; i < M ; i++)
+  value_t **mat = (value_t **)malloc(sizeof(value_t *) * M);
+  value_t *data = (value_t *)malloc(N * M * sizeof(value_t));
+  for (int i = 0; i < M; i++)
   {
-    mat[i] = malloc(sizeof(value_t) * N);
+    mat[i] = &(data[N * i]);
   }
   return mat;
 }
 
-void releaseMatrix(Matrix m) { free(m); }
+void releaseMatrix(Matrix m)
+{
+  free(m[0]);
+}
 void releaseRoom(Room m) { free(m); }
 
 int *getPlaneSizePerRank(int H, int number_of_ranks)
@@ -387,7 +401,7 @@ int *getPlaneSizePerRank(int H, int number_of_ranks)
   for (int loop = 0; loop < number_of_ranks; loop++)
   {
     planes_per_rank[loop] = whole_div;
-    if (loop == 0) 
+    if (loop == 0)
       planes_per_rank[loop] += H % number_of_ranks;
   }
   return planes_per_rank;
@@ -404,8 +418,8 @@ void printTemperature(Room m, int N, int M, int H)
 
   // set the 'render' resolution
   int Xres = RESOLUTION;
-  int Yres = RESOLUTION/4;
-  int Zres = RESOLUTION/20;
+  int Yres = RESOLUTION / 4;
+  int Zres = RESOLUTION / 20;
   // printf("Resolution = %d x %d x %d\n",Xres,Yres,Zres);
   // step size in each dimension
   int xW = N / Xres;
@@ -418,7 +432,7 @@ void printTemperature(Room m, int N, int M, int H)
     // top wall
     for (int j = 0; j < Xres; j++)
     {
-      if (j == 0 || j == Xres-1)
+      if (j == 0 || j == Xres - 1)
         printf("+");
       else
         printf("-");
@@ -458,7 +472,7 @@ void printTemperature(Room m, int N, int M, int H)
     // bottom wall
     for (int j = 0; j < Xres; j++)
     {
-      if (j == 0 || j == Xres-1)
+      if (j == 0 || j == Xres - 1)
         printf("+");
       else
         printf("-");
