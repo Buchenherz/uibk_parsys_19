d822300d2605c608913ef691295b5c509bdfb592
Clemens Pfister <clemens.pfister@student.uibk.ac.at>
2019-11-28 15:58:52 +0100

    fixes for undefined behaviour warning

--- proseminar/08/ex1/pi_seq.c
+++ proseminar/08/ex1/pi_seq.c
@@ -20,17 +20,17 @@ int main(int argc, char *argv[]) {
     }
     // endregion
     int hits = 0;
-    int gen = 0;
-    for (gen; gen < MAX_GEN_ELEMENTS; gen++) {
+    for (int gen = 0; gen < MAX_GEN_ELEMENTS; gen++) {
         // https://itp.tugraz.at/MML/MonteCarlo/MCIntro.pdf
         double random_x = (double)rand() / RAND_MAX;  // float in range 0 to 1
         double random_y = (double)rand() / RAND_MAX;
         double random_point = pow(random_x, 2) + pow(random_y, 2);
-        hits = (random_point < 1) ? ++hits : hits;
+        if (random_point < 1) {
+            hits++;
+        }
     }
-    double pi = ((double)hits / (double)gen) * 4;
+    double pi = ((double)hits / (double)MAX_GEN_ELEMENTS) * 4;
     double end_time = omp_get_wtime();
-    printf("Gen =  %d\n", gen);
-    printf("PI =  %f\n", pi);
-    printf("Walltime =  %f\n", end_time - start_time);
+    printf("PI, Walltime\n");
+    printf("%f, %f\n", pi, end_time - start_time);
 }
