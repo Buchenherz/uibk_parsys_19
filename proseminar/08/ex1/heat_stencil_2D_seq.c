1adf08a591d224cd02800be0f79e836ea7424603
Clemens Pfister <clemens.pfister@student.uibk.ac.at>
2019-11-28 15:58:40 +0100

    quick`n`dirty heat stencil omp version

--- proseminar/08/ex1/heat_stencil_2D_seq.c
+++ proseminar/08/ex1/heat_stencil_2D_seq.c
@@ -1,11 +1,13 @@
+#include <omp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 
 typedef double value_t;
 
 #define RESOLUTION 100
+#define VERBOSE
 
 // -- Matrix utilities --
 
@@ -19,193 +21,174 @@ void printTemperature(Matrix m, int N, int M);
 
 // -- simulation code ---
 
-int main(int argc, char **argv)
-{
-  // 'parsing' optional input parameter = problem size
-  int N = 200; // columns
-  int M = 200; // rows
-  clock_t time;
-  bool print = true;
+int main(int argc, char **argv) {
+    // 'parsing' optional input parameter = problem size
+    int N = 200;  // columns
+    int M = 200;  // rows
 
-  if (argc > 1)
-  {
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
-    if (argc > 3)
-    {
-      print = false;
+    if (argc > 1) {
+        N = atoi(argv[1]);
+        M = atoi(argv[2]);
     }
-  }
-  int T = (N < M ? M : N) * 500;
-  printf("Computing heat-distribution for room size N=%d, M=%d for T=%d timesteps\n", N, M, T);
-  time = clock();
-  // ---------- setup ----------
+    int T = (N < M ? M : N) * 500;
+#ifdef VERBOSE
+    printf(
+        "Computing heat-distribution for room size N=%d, M=%d for T=%d "
+        "timesteps\n",
+        N, M, T);
+#endif
+    double start_time = omp_get_wtime();
+    // ---------- setup ----------
 
-  // create a buffer for storing temperature fields
-  Matrix A = createMatrix(N, M);
+    // create a buffer for storing temperature fields
+    Matrix A = createMatrix(N, M);
 
-  // set up initial conditions in A
-  for (int i = 0; i < M; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
-      A[i][j] = 273; // temperature is 0° C everywhere (273 K)
+    // set up initial conditions in A
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = 273;  // temperature is 0° C everywhere (273 K)
+        }
     }
-  }
 
-  // and there is a heat source in one corner
-  int source_x = N / 4;
-  int source_y = M / 8;
-  A[source_y][source_x] = 273 + 60;
+    // and there is a heat source in one corner
+    int source_x = N / 4;
+    int source_y = M / 8;
+    A[source_y][source_x] = 273 + 60;
 
-  if (print)
-  {
+#ifdef VERBOSE
     printf("Initial:\t");
     printTemperature(A, N, M);
     printf("\n");
-  }
+#endif
 
-  // ---------- compute ----------
+    // ---------- compute ----------
 
-  // create a second buffer for the computation
-  Matrix B = createMatrix(N, M);
+    // create a second buffer for the computation
+    Matrix B = createMatrix(N, M);
 
-  // for each time step ..
-  for (int t = 0; t < T; t++)
-  {
-    // .. we propagate the temperature
-    for (long long i = 0; i < M; i++)
-    {
-      for (long long j = 0; j < N; j++)
-      {
-        // center stays constant (the heat is still on)
-        if (i == source_y && j == source_x)
-        {
-          B[i][j] = A[i][j];
-          continue;
+    // for each time step ..
+    for (int t = 0; t < T; t++) {
+        // .. we propagate the temperature
+        for (long long i = 0; i < M; i++) {
+            for (long long j = 0; j < N; j++) {
+                // center stays constant (the heat is still on)
+                if (i == source_y && j == source_x) {
+                    B[i][j] = A[i][j];
+                    continue;
+                }
+
+                // get temperature at current position
+                value_t tc = A[i][j];
+
+                // get temperatures of adjacent cells
+                value_t tl = (j != 0) ? A[i][j - 1] : tc;
+                value_t tr = (j != N - 1) ? A[i][j + 1] : tc;
+                value_t tu = (i != 0) ? A[i - 1][j] : tc;
+                value_t td = (i != M - 1) ? A[i + 1][j] : tc;
+
+                // compute new temperature at current position
+                B[i][j] = tc + 0.2 * (tl + tr + tu + td + (-4 * tc));
+            }
         }
 
-        // get temperature at current position
-        value_t tc = A[i][j];
+        // swap matrices (just pointers, not content)
+        Matrix H = A;
+        A = B;
+        B = H;
 
-        // get temperatures of adjacent cells
-        value_t tl = (j != 0) ? A[i][j - 1] : tc;
-        value_t tr = (j != N - 1) ? A[i][j + 1] : tc;
-        value_t tu = (i != 0) ? A[i - 1][j] : tc;
-        value_t td = (i != M - 1) ? A[i + 1][j] : tc;
-
-        // compute new temperature at current position
-        B[i][j] = tc + 0.2 * (tl + tr + tu + td + (-4 * tc));
-      }
+#ifdef VERBOSE
+        // show intermediate step
+        if (!(t % 1000)) {
+            printf("Step t=%d:\n", t);
+            printTemperature(A, N, M);
+            printf("\n");
+        }
+#endif
     }
 
-    // swap matrices (just pointers, not content)
-    Matrix H = A;
-    A = B;
-    B = H;
+    releaseMatrix(B);
 
-    // show intermediate step
-    if (!(t % 1000) && print)
-    {
-      printf("Step t=%d:\n", t);
-      printTemperature(A, N, M);
-      printf("\n");
-    }
-  }
-
-  releaseMatrix(B);
-
-  // ---------- check ----------
-  if (print)
-  {
+    // ---------- check ----------
+#ifdef VERBOSE
     printf("Final:\n");
     printTemperature(A, N, M);
     printf("\n");
-  }
+#endif
 
-  int success = 1;
-  for (long long i = 0; i < M; i++)
-  {
-    for (long long j = 0; j < N; j++)
-    {
-      value_t temp = A[i][j];
-      if (273 <= temp && temp <= 273 + 60)
-        continue;
-      success = 0;
-      break;
+    int success = 1;
+    for (long long i = 0; i < M; i++) {
+        for (long long j = 0; j < N; j++) {
+            value_t temp = A[i][j];
+            if (273 <= temp && temp <= 273 + 60) continue;
+            success = 0;
+            break;
+        }
     }
-  }
+#ifdef VERBOSE
+    printf("Verification: %s\n", (success) ? "OK" : "FAILED");
+#endif
+    // ---------- cleanup ----------
 
-  printf("Verification: %s\n", (success) ? "OK" : "FAILED");
+    releaseMatrix(A);
+    double end_time = omp_get_wtime();
+    double time_taken = end_time - start_time;
+    printf("Walltime, M, N\n");
+    printf("%f, %d, %d\n", time_taken, M, N);
 
-  // ---------- cleanup ----------
-
-  releaseMatrix(A);
-  time = clock() - time;
-  double time_taken = ((double)time) / CLOCKS_PER_SEC; // in seconds
-  printf("2d MPI heat stencil took %f seconds to execute \n", time_taken);
-
-  // done
-  return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
+    // done
+    return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-Matrix createMatrix(int N, int M)
-{
-  // create data and index Matrix
-  value_t **mat = malloc(sizeof(value_t) * M);
-  for (int i = 0; i < M; i++)
-  {
-    mat[i] = malloc(sizeof(value_t) * N);
-  }
-  return mat;
+Matrix createMatrix(int N, int M) {
+    // create data and index Matrix
+    value_t **mat = malloc(sizeof(value_t) * M);
+    for (int i = 0; i < M; i++) {
+        mat[i] = malloc(sizeof(value_t) * N);
+    }
+    return mat;
 }
 
 void releaseMatrix(Matrix m) { free(m); }
 
-void printTemperature(Matrix m, int N, int M)
-{
-  const char *colors = " .-:=+*^X#%@";
-  const int numColors = 12;
+void printTemperature(Matrix m, int N, int M) {
+    const char *colors = " .-:=+*^X#%@";
+    const int numColors = 12;
 
-  // boundaries for temperature (for simplicity hard-coded)
-  const value_t max = 273 + 30;
-  const value_t min = 273 + 0;
+    // boundaries for temperature (for simplicity hard-coded)
+    const value_t max = 273 + 30;
+    const value_t min = 273 + 0;
 
-  // set the 'render' resolution
-  int W = RESOLUTION;
-  int H = RESOLUTION / 4;
+    // set the 'render' resolution
+    int W = RESOLUTION;
+    int H = RESOLUTION / 4;
 
-  // step size in each dimension
-  int xW = N / W;
-  int yW = M / H;
+    // step size in each dimension
+    int xW = N / W;
+    int yW = M / H;
 
-  // room
-  for (int i = 0; i < H; i++)
-  {
-    // left wall
-    printf("X");
-    // actual room
-    for (int j = 0; j < W; j++)
-    {
-      // get max temperature in this tile
-      value_t max_t = 0;
-      for (int y = yW * i; y < yW * i + yW; y++)
-      {
-        for (int x = xW * j; x < xW * j + xW; x++)
-        {
-          max_t = (max_t < m[y][x]) ? m[y][x] : max_t;
+    // room
+    for (int i = 0; i < H; i++) {
+        // left wall
+        printf("X");
+        // actual room
+        for (int j = 0; j < W; j++) {
+            // get max temperature in this tile
+            value_t max_t = 0;
+            for (int y = yW * i; y < yW * i + yW; y++) {
+                for (int x = xW * j; x < xW * j + xW; x++) {
+                    max_t = (max_t < m[y][x]) ? m[y][x] : max_t;
+                }
+            }
+            value_t temp = max_t;
+
+            // pick the 'color'
+            int c = ((temp - min) / (max - min)) * numColors;
+            c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
+
+            // print the average temperature
+            printf("%c", colors[c]);
         }
-      }
-      value_t temp = max_t;
-
-      // pick the 'color'
-      int c = ((temp - min) / (max - min)) * numColors;
-      c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
-
-      // print the average temperature
-      printf("%c", colors[c]);
+        // right wall
+        printf("X\n");
     }
-    // right wall
-    printf("X\n");
-  }
 }
